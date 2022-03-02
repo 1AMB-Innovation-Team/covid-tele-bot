e9de3b8ae41c25f413efacec8383d0d28c26fae9
theliewasacake <jc050402@gmail.com>
2022-03-03 01:05:54 +0800

    d

--- bot.py
+++ bot.py
@@ -560,8 +560,16 @@ def main() -> None:
     #Errors
     dispatcher.add_error_handler(error_handler)
 
+    # Set these variable to the appropriate values
+    TOKEN = "5249938330:AAF4vd0RX2Jdx_24vxDz3lSSWPUSWOOLRic"
+    HNAME = "covidtracker-bot-12"
+
+    # Port is given by Heroku
+    PORT = os.environ.get('PORT')
+
     # Start the Bot
-    # updater.start_polling()# Start the webhook
+    # updater.start_polling()
+    # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN,
