ac5459c66b0587dba2694ec533b2aceda1124d60
theliewasacake <jc050402@gmail.com>
2022-03-03 00:29:15 +0800

    test

--- covid_tracker.py
+++ covid_tracker.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = 'YOURTELEGRAMBOTTOKEN'
+TOKEN = '5249938330:AAF4vd0RX2Jdx_24vxDz3lSSWPUSWOOLRic'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -56,10 +56,13 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
+    
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://covidtracker-bot-12.herokuapp.com/' + TOKEN)
+    
+    # updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
