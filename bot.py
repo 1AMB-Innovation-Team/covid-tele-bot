965a219079ec39bbe330117dba9b0d2bf6434393
theliewasacake <jc050402@gmail.com>
2022-03-03 01:10:34 +0800

    testffff

--- bot.py
+++ bot.py
@@ -503,7 +503,7 @@ def main() -> None:
     PORT = os.environ.get('PORT')
     
     pers = PicklePersistence(filename='case_list.pkl')
-    updater = Updater(TOKEN, persistence=pers)
+    updater = Updater(TOKEN)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
