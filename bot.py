f098506576b665426fb193392021d07539d43601
theliewasacake <jc050402@gmail.com>
2022-03-03 09:56:31 +0800

    pls work now

--- bot.py
+++ bot.py
@@ -513,9 +513,9 @@ def main() -> None:
     PORT = os.environ.get('PORT')
     '''Run the bot.'''
     # Create the Updater and pass it your bot's token.
-    DATABASE_URL = os.environ['DATABASE_URL']
-
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    DATABASE_URL = os.environ['DATABASE_URL'] #'postgresql://fcoiknwokizqha:27e5045aa7b0291c9b91ab044b91ddd2ff8e06be37946ae5c09adfc570da57f3@ec2-3-225-79-57.compute-1.amazonaws.com:5432/d7nrhv19m54qjk'  
+    DATABASE_URL.replace('postgres','postgresql',1)
+    # conn = psycopg2.connect(dbname='d7nrhv19m54qjk', host='ec2-3-225-79-57.compute-1.amazonaws.com', port=5432, user='fcoiknwokizqha', password='27e5045aa7b0291c9b91ab044b91ddd2ff8e06be37946ae5c09adfc570da57f3', sslmode='require')
     
     pers = PostgresPersistence(url=DATABASE_URL)
     updater = Updater(TOKEN, persistence=pers)
